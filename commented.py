ed48d7b5ed793bb198e27f4fc9e6f5c525169145
mermer03 <109605191+yuchenz03@users.noreply.github.com>
2024-05-15 11:24:20 +0400

    added stuff to commented.py

--- commented.py
+++ commented.py
@@ -1,243 +1,101 @@
-##### CODE COMMENTED ON PAPER #####
-
 import random
 import os
 
-def Main(): 
-    Again = "y" #set again to equal yes initially to start the first run
-    Score = 0 #set score to equal 0
-    while Again == "y": #if again is set to True
-        Filename = input("Press Enter to start a standard puzzle or enter name of file to load: ") #to load a  file
-        if len(Filename) > 0: #If a puzzle is to be opened, then:
-            MyPuzzle = Puzzle(Filename + ".txt") #Open the file with the specified name
-        else: #otherwise, load the standard puzzle
-            MyPuzzle = Puzzle(8, int(8 * 8 * 0.6)) #the size of the puzzle is 8x8, and the number of                 
-        Score = MyPuzzle.AttemptPuzzle() #Calling the AttemptPuzzle class, which returns the score
-        print("Puzzle finished. Your score was: " + str(Score)) #Outputs the score achieved 
-        Again = input("Do another puzzle? ").lower() #Asks the user if they want to do another puzzle
+'''
+Some general comments:
+All loops start from 1 and end with + 1, most likely since it was converted from
+C to python.
+'''
 
-class Puzzle(): 
-    def __init__(self, *args):  #Initializing the Puzzle object
-        if len(args) == 1: #if there is only one argument passed - a puzzle file will be loaded
-            self.__Score = 0 #sets score to 0
-            self.__SymbolsLeft = 0 #initializes symbols left variable
-            self.__GridSize = 0 #initializes gridsize variable
-            self.__Grid = [] #initializes grid
-            self.__AllowedPatterns = [] #initializes allowed patterns variable
-            self.__AllowedSymbols = [] #initializes allowed symbols variable
-            self.__LoadPuzzle(args[0]) #Load the puzzle using the LoadPuzzle method.
-        else: #Otherwise, if the standard puzzle is to be loaded
-            self.__Score = 0 #initialize score
-            self.__SymbolsLeft = args[1] #number of symbols to be placed is second argument passed
-            self.__GridSize = args[0] #grid size is the first argument passed
-            self.__Grid = [] #initializes grid
-            for Count in range(1, self.__GridSize * self.__GridSize + 1): #
-                if random.randrange(1, 101) < 90: #with a 90% chance of creating a cell
-                    C = Cell() #Creating an object Cell
-                else: #10% chance of creating a blocked cell.
-                    C = BlockedCell() #Creating an object blocked cell
-                self.__Grid.append(C) #add it to the grid
-            self.__AllowedPatterns = [] #Initialize list of allowed patterns
-            self.__AllowedSymbols = [] #Initialize list of allowed symbols
-            QPattern = Pattern("Q", "QQ**Q**QQ") #Create the Q Pattern object
+def Main():
+    Again = "y"
+    Score = 0
+    while Again == "y":
+        Filename = input("Press Enter to start a standard puzzle or enter name of file to load: ")
+        if len(Filename) > 0:
+            MyPuzzle = Puzzle(Filename+".txt") 
+            #creating a puzzle instance that opens a pre-designed game
+        else:
+            MyPuzzle = Puzzle(8, int(8*8*0.6))
+            #creating a puzzle instance that creates a new general game
+        Score = MyPuzzle.AttemptPuzzle() 
+        #Since the method AttemptPuzzle() returns the final score of the game,
+        #we can set a variable named score to the method AttemptPuzzle()
+        print("Puzzle finished. Your score was: " + str(Score))
+        #the variable score is an integer, so (even though you don't have to 
+        # in python) cast it to an integer and print it
+        Again = input("Do another puzzle? ").lower() 
+        #validates for capital letter Y rather than y
+        
+class Puzzle():
+    def __init__(self, *args):
+    #the puzzle class created can have any number of arguments, but in this
+    #code it will only ever have 1 or 2 arguments.
+        if len(args) == 1:
+        #if there is only one argument - if a pre-made puzzle is to be made
+        
+        #note that the following variables have all been initialized, but their
+        #values are assigned in the __LoadPuzzle method
+            self.__Score = 0
+            self.__SymbolsLeft = 0
+            self.__GridSize = 0
+            self.__Grid = []
+            #Note how the grid is a one-dimensional list, not two.
+            #This later causes issues with validation of whether a symbol is
+            #completed or not (see notes for demonstration)
+            self.__AllowedPatterns = []
+            self.__AllowedSymbols = []
+            #The allowed patterns and allowed symbols variables allow the user
+            #add different types of symbols to score points. These allowed
+            #symbols and patterns are specified in the file
+            self.__LoadPuzzle(args[0])
+            #the load puzzle function is used specifically for loading pre-made
+            #puzzle files, where the argument passed in is the name of the file
+            #to be loaded
+
+        else:
+        #if there are two arguments - if the standard puzzle is to be made
+            self.__Score = 0
+            self.__SymbolsLeft = args[0]
+            self.__GridSize = args[1]
+            #note that this is one side of the grid, not the total grid area.
+            self.__Grid = []
+            for Count in range(1, self.__GridSize * self.__GridSize + 1):
+            #looping through the entire grid (most likely starting at 1 because
+            #converted from C)
+                if random.randrange(1,101) < 90:
+                #remember that the randrange function is inclusive, exclusive
+                #for any given cell, there is a 90% change that it is a normal
+                #cell, and 10% chance that it is a blocked cell
+                    C = Cell()
+                else:
+                    C = BlockedCell()
+                self.__Grid.append(C)
+                #After creating a cell, append it to the grid
+            self.__AllowedPatterns = []
+            self.__AllowedSymbols = []
+            #populating the above lists below
+            QPattern = Pattern("Q", "QQ**Q**QQ")
+            #Creating a new instance of the Pattern class
             self.__AllowedPatterns.append(QPattern)
-            self.__AllowedSymbols.append("Q") 
-            XPattern = Pattern("X", "X*X*X*X*X") #Create the X Pattern object
+            #Appending the pattern instance to the allowed patterns class
+            self.__AllowedSymbols.append("Q")
+            #Appending the symbol as a string to the allowed symbols class
+            XPattern = Pattern("X", "X*X*X*X*X")
             self.__AllowedPatterns.append(XPattern)
             self.__AllowedSymbols.append("X")
-            TPattern = Pattern("T", "TTT**T**T") #Create the T Pattern object
+            TPattern = Pattern("T", "TTT**T**T")
             self.__AllowedPatterns.append(TPattern)
             self.__AllowedSymbols.append("T")
-
-    def __LoadPuzzle(self, Filename):
+        
+    def __LoadPuzzle(self, Filename): 
+    #this method is only used when playing a pre-made puzzle
         try:
             with open(Filename) as f:
                 NoOfSymbols = int(f.readline().rstrip())
-                for Count in range (1, NoOfSymbols + 1):
-                    self.__AllowedSymbols.append(f.readline().rstrip())
-                NoOfPatterns = int(f.readline().rstrip())
-                for Count in range(1, NoOfPatterns + 1):
-                    Items = f.readline().rstrip().split(",")
-                    P = Pattern(Items[0], Items[1])
-                    self.__AllowedPatterns.append(P)
-                self.__GridSize = int(f.readline().rstrip())
-                for Count in range (1, self.__GridSize * self.__GridSize + 1):
-                    Items = f.readline().rstrip().split(",")
-                    if Items[0] == "@":
-                        C = BlockedCell()
-                        self.__Grid.append(C)
-                    else:
-                        C = Cell()
-                        C.ChangeSymbolInCell(Items[0])
-                        for CurrentSymbol in range(1, len(Items)):
-                            C.AddToNotAllowedSymbols(Items[CurrentSymbol])
-                        self.__Grid.append(C)
-                self.__Score = int(f.readline().rstrip())
-                self.__SymbolsLeft = int(f.readline().rstrip())
-        except:
-            print("Puzzle not loaded")
-
-    def AttemptPuzzle(self):
-        Finished = False
-        while not Finished:
-            self.DisplayPuzzle()
-            print("Current score: " + str(self.__Score))
-            Row = -1
-            Valid = False
-            while not Valid:
-                try:
-                    Row = int(input("Enter row number: "))
-                    Valid = True
-                except:
-                    pass
-            Column = -1
-            Valid = False
-            while not Valid:
-                try:
-                    Column = int(input("Enter column number: "))
-                    Valid = True
-                except:
-                    pass
-            Symbol = self.__GetSymbolFromUser()
-            self.__SymbolsLeft -= 1
-            CurrentCell = self.__GetCell(Row, Column)
-            if CurrentCell.CheckSymbolAllowed(Symbol):
-                CurrentCell.ChangeSymbolInCell(Symbol)
-                AmountToAddToScore = self.CheckforMatchWithPattern(Row, Column)
-                if AmountToAddToScore > 0:
-                    self.__Score += AmountToAddToScore
-            if self.__SymbolsLeft == 0:
-                Finished = True
-        print()
-        self.DisplayPuzzle()
-        print()
-        return self.__Score
-
-    def __GetCell(self, Row, Column):
-        Index = (self.__GridSize - Row) * self.__GridSize + Column - 1
-        if Index >= 0:
-            return self.__Grid[Index]
-        else:
-            raise IndexError()
-
-    def CheckforMatchWithPattern(self, Row, Column):
-        for StartRow in range(Row + 2, Row - 1, -1):
-            for StartColumn in range(Column - 2, Column + 1):
-                try:
-                    PatternString = ""
-                    PatternString += self.__GetCell(StartRow, StartColumn).GetSymbol()
-                    PatternString += self.__GetCell(StartRow, StartColumn + 1).GetSymbol()
-                    PatternString += self.__GetCell(StartRow, StartColumn + 2).GetSymbol()
-                    PatternString += self.__GetCell(StartRow - 1, StartColumn + 2).GetSymbol()
-                    PatternString += self.__GetCell(StartRow - 2, StartColumn + 2).GetSymbol()
-                    PatternString += self.__GetCell(StartRow - 2, StartColumn + 1).GetSymbol()
-                    PatternString += self.__GetCell(StartRow - 2, StartColumn).GetSymbol()
-                    PatternString += self.__GetCell(StartRow - 1, StartColumn).GetSymbol()
-                    PatternString += self.__GetCell(StartRow - 1, StartColumn + 1).GetSymbol()
-                    for P in self.__AllowedPatterns:
-                        CurrentSymbol = self.__GetCell(Row, Column).GetSymbol()
-                        if P.MatchesPattern(PatternString, CurrentSymbol):
-                            self.__GetCell(StartRow, StartColumn).AddToNotAllowedSymbols(CurrentSymbol)
-                            self.__GetCell(StartRow, StartColumn + 1).AddToNotAllowedSymbols(CurrentSymbol)
-                            self.__GetCell(StartRow, StartColumn + 2).AddToNotAllowedSymbols(CurrentSymbol)
-                            self.__GetCell(StartRow - 1, StartColumn + 2).AddToNotAllowedSymbols(CurrentSymbol)
-                            self.__GetCell(StartRow - 2, StartColumn + 2).AddToNotAllowedSymbols(CurrentSymbol)
-                            self.__GetCell(StartRow - 2, StartColumn + 1).AddToNotAllowedSymbols(CurrentSymbol)
-                            self.__GetCell(StartRow - 2, StartColumn).AddToNotAllowedSymbols(CurrentSymbol)
-                            self.__GetCell(StartRow - 1, StartColumn).AddToNotAllowedSymbols(CurrentSymbol)
-                            self.__GetCell(StartRow - 1, StartColumn + 1).AddToNotAllowedSymbols(CurrentSymbol)
-                            return 10
-                except: 
-                    pass 
-        return 0 
-
-    def __GetSymbolFromUser(self): 
-        Symbol = ""
-        while not Symbol in self.__AllowedSymbols:
-            Symbol = input("Enter symbol: ")
-        return Symbol
-
-    def __CreateHorizontalLine(self):
-        Line = "  "
-        for Count in range(1, self.__GridSize * 2 + 2):
-            Line = Line + "-"
-        return Line
-
-    def DisplayPuzzle(self):
-        print()
-        if self.__GridSize < 10:
-            print("  ", end='')
-            for Count in range(1, self.__GridSize + 1):
-                print(" " + str(Count), end='')
-        print()
-        print(self.__CreateHorizontalLine())
-        for Count in range(0, len(self.__Grid)):
-            if Count % self.__GridSize == 0 and self.__GridSize < 10:
-                print(str(self.__GridSize - ((Count + 1) // self.__GridSize)) + " ", end='')
-            print("|" + self.__Grid[Count].GetSymbol(), end='')
-            if (Count + 1) % self.__GridSize == 0:
-                print("|")
-                print(self.__CreateHorizontalLine())
-
-class Pattern():
-    def __init__(self, SymbolToUse, PatternString):
-        self.__Symbol = SymbolToUse
-        self.__PatternSequence = PatternString
-
-    def MatchesPattern(self, PatternString, SymbolPlaced):
-        if SymbolPlaced != self.__Symbol:
-            return False
-        for Count in range(0, len(self.__PatternSequence)):
-            try:
-                if self.__PatternSequence[Count] == self.__Symbol and PatternString[Count] != self.__Symbol:
-                    return False
-            except Exception as ex:
-                print(f"EXCEPTION in MatchesPattern: {ex}")
-        return True
-
-    def GetPatternSequence(self):
-      return self.__PatternSequence
-
-class Cell():
-    def __init__(self):
-        self._Symbol = ""
-        self.__SymbolsNotAllowed = []
-
-    def GetSymbol(self):
-        if self.IsEmpty():
-          return "-"
-        else:
-          return self._Symbol
-    
-    def IsEmpty(self):
-        if len(self._Symbol) == 0:
-            return True
-        else:
-            return False
-
-    def ChangeSymbolInCell(self, NewSymbol):
-        self._Symbol = NewSymbol
-
-    def CheckSymbolAllowed(self, SymbolToCheck):
-        for Item in self.__SymbolsNotAllowed:
-            if Item == SymbolToCheck:
-                return False
-        return True
-
-    def AddToNotAllowedSymbols(self, SymbolToAdd):
-        self.__SymbolsNotAllowed.append(SymbolToAdd)
-
-    def UpdateCell(self):
-        pass
-
-class BlockedCell(Cell): 
-    def __init__(self):
-        super(BlockedCell, self).__init__()
-        self._Symbol = "@"
-
-    def CheckSymbolAllowed(self, SymbolToCheck):
-        return False
-
-if __name__ == "__main__":
-    Main()
-    
+                #the first line contains the number of symbols to be appended 
+                #into allowed symbols
+                for Count in range(1, NoOfSymbols + 1):
+                #looping through each line that has a symbol to be appended
+                
+                
